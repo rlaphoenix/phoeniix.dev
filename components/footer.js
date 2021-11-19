d6cb7e76540fb9f0f10c72b5abea1cd660442011
rlaphoenix <pragma.exe@gmail.com>
2021-11-19 18:27:56 +0000

    fix: Replace non-existent /contact link in footer with email

--- components/footer.js
+++ components/footer.js
@@ -12,7 +12,7 @@ function Copyright({begin}) {
 export default function Footer() {
     return <footer className="flex flex-col p-8 gap-4 border-t border-gray-200">
         <div className="flex flex-row flex-wrap justify-center gap-x-16 gap-y-4 text-gray-500">
-            <Link href={"/contact"}><a>Contact</a></Link>
+            <Link href={"mailto:rlaphoenix@pm.me"}><a>Contact</a></Link>
             <Link href={"https://media.phoeniix.dev:8443"}><a>Emby</a></Link>
             <Link href={"https://media.phoeniix.dev:2096"}><a>Plex</a></Link>
         </div>
