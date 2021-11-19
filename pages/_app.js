6e8b0659b9acd368a1ce2a07a6daf1cff5bafbf2
rlaphoenix <pragma.exe@gmail.com>
2021-11-19 12:33:31 +0000

    Use navigation component on all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 require("windi.css")
 
 import Head from "next/head"
+import Navigation from "../components/navigation"
 import Footer from "../components/footer"
 
 export default function App({Component, pageProps}) {
@@ -10,6 +11,7 @@ export default function App({Component, pageProps}) {
             <meta charSet="utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
         </Head>
+        <Navigation Emby="https://media.phoeniix.dev:8443" Plex="https://media.phoeniix.dev:2096" />
         <main>
             <div className="max-w-4xl mx-auto p-4 sm:p-6 lg:px-8">
                 <div className="gap-2">
